82ea9199b8d5b4782c2d1d3c2de15f36da563aee
Ahtsham Raziq <ahtsham.raziq@filestage.io>
2020-11-02 04:50:47 +0500

    hide annotations toolbar after 3 seconds of inactivity

--- src/components/AnnotationToolsOverlay/AnnotationToolsOverlay.js
+++ src/components/AnnotationToolsOverlay/AnnotationToolsOverlay.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import ToolButton from 'components/ToolButton';
-import './AnnotationToolsOverlay.scss';
+import core from "core";
+import selectors from "selectors";
+import ToolButton from "components/ToolButton";
+import "./AnnotationToolsOverlay.scss";
+
+const MAX_INACTIVITY_MS = 3000;
 
 const AnnotationToolsOverlay = () => {
-  return (
-    <div className="Overlay AnnotationToolsOverlay" data-element="annotationToolsOverlay">
-      <ToolButton
-        toolName="AnnotationCreateTextHighlight"
-      />
-      <ToolButton
-        toolName="AnnotationCreateTextStrikeout"
-      />
-      <ToolButton
-        toolName="AnnotationCreateSticky"
-      />
+  const isDisabled = useSelector((state) =>
+    selectors.isElementDisabled(state, "annotationToolsOverlay")
+  );
+
+  const [timeoutId, setTimeoutId] = useState(undefined);
+  const [isHiddenDueToInactivity, setIsHiddenDueToInactivity] = useState(false);
+
+  useEffect(() => {
+    const onMouseHover = (e) => {
+      setIsHiddenDueToInactivity(false);
+
+      if (typeof timeoutId === "number") {
+        clearTimeout(timeoutId);
+      }
+
+      setTimeoutId(
+        setTimeout(() => {
+          setIsHiddenDueToInactivity(true), setTimeoutId(undefined);
+        }, MAX_INACTIVITY_MS)
+      );
+    };
+
+    core.addEventListener("mouseMove", onMouseHover);
+    return () => {
+      if (typeof timeoutId === "number") {
+        clearTimeout(timeoutId);
+      }
+
+      core.removeEventListener("mouseMove", onMouseHover);
+    };
+  }, [timeoutId]);
+
+  return isDisabled || isHiddenDueToInactivity ? null : (
+    <div
+      className="Overlay AnnotationToolsOverlay"
+      data-element="annotationToolsOverlay"
+    >
+      <ToolButton toolName="AnnotationCreateTextHighlight" />
+      <ToolButton toolName="AnnotationCreateTextStrikeout" />
+      <ToolButton toolName="AnnotationCreateSticky" />
     </div>
   );
 };
