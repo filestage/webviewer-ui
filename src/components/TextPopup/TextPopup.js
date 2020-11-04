eb63509c44a188bf928535046763400ea259c63f
Ahtsham Raziq <ahtsham.raziq@filestage.io>
2020-11-05 04:29:12 +0500

    prettier

--- src/components/TextPopup/TextPopup.js
+++ src/components/TextPopup/TextPopup.js
@@ -1,58 +1,58 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useSelector, useDispatch, shallowEqual } from 'react-redux';
-import classNames from 'classnames';
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
+import classNames from "classnames";
 
-import ActionButton from 'components/ActionButton';
-import CustomizablePopup from 'components/CustomizablePopup';
+import ActionButton from "components/ActionButton";
+import CustomizablePopup from "components/CustomizablePopup";
 
-import core from 'core';
-import { getTextPopupPositionBasedOn } from 'helpers/getPopupPosition';
-import createTextAnnotationAndSelect from 'helpers/createTextAnnotationAndSelect';
-import copyText from 'helpers/copyText';
-import useOnClickOutside from 'hooks/useOnClickOutside';
-import actions from 'actions';
-import selectors from 'selectors';
+import core from "core";
+import { getTextPopupPositionBasedOn } from "helpers/getPopupPosition";
+import createTextAnnotationAndSelect from "helpers/createTextAnnotationAndSelect";
+import copyText from "helpers/copyText";
+import useOnClickOutside from "hooks/useOnClickOutside";
+import actions from "actions";
+import selectors from "selectors";
 
-import './TextPopup.scss';
+import "./TextPopup.scss";
 
 const TextPopup = () => {
   const [isDisabled, isOpen, popupItems, activeTextAnnotation] = useSelector(
-    state => [
-      selectors.isElementDisabled(state, 'textPopup'),
-      selectors.isElementOpen(state, 'textPopup'),
-      selectors.getPopupItems(state, 'textPopup'),
+    (state) => [
+      selectors.isElementDisabled(state, "textPopup"),
+      selectors.isElementOpen(state, "textPopup"),
+      selectors.getPopupItems(state, "textPopup"),
       selectors.getActiveTextAnnotation(state),
     ],
-    shallowEqual,
+    shallowEqual
   );
   const dispatch = useDispatch();
   const [position, setPosition] = useState({ left: 0, top: 0 });
   const popupRef = useRef();
   useOnClickOutside(popupRef, () => {
-    dispatch(actions.closeElement('textPopup'));
+    dispatch(actions.closeElement("textPopup"));
   });
 
   useEffect(() => {
     if (isOpen) {
-      dispatch(actions.closeElements(['annotationPopup', 'contextMenuPopup']));
+      dispatch(actions.closeElements(["annotationPopup", "contextMenuPopup"]));
     }
   }, [dispatch, isOpen]);
 
   useEffect(() => {
-    const textSelectTool = core.getTool('TextSelect');
+    const textSelectTool = core.getTool("TextSelect");
 
     const onSelectionComplete = (startQuad, allQuads) => {
       if (popupRef.current && popupItems.length > 0) {
         setPosition(getTextPopupPositionBasedOn(allQuads, popupRef));
-        dispatch(actions.openElement('textPopup'));
+        dispatch(actions.openElement("textPopup"));
       }
     };
 
-    textSelectTool.on('selectionComplete', onSelectionComplete);
-    return () => textSelectTool.off('selectionComplete', onSelectionComplete);
+    textSelectTool.on("selectionComplete", onSelectionComplete);
+    return () => textSelectTool.off("selectionComplete", onSelectionComplete);
   }, [dispatch, popupItems]);
 
-  const isActive = textAnnotation => activeTextAnnotation === textAnnotation;
+  const isActive = (textAnnotation) => activeTextAnnotation === textAnnotation;
 
   return isDisabled ? null : (
     <div
@@ -80,7 +80,7 @@ const TextPopup = () => {
           onClick={() =>
             createTextAnnotationAndSelect(
               dispatch,
-              Annotations.TextHighlightAnnotation,
+              Annotations.TextHighlightAnnotation
             )
           }
           isActive={isActive("highlight")}
@@ -92,7 +92,7 @@ const TextPopup = () => {
           onClick={() =>
             createTextAnnotationAndSelect(
               dispatch,
-              Annotations.TextUnderlineAnnotation,
+              Annotations.TextUnderlineAnnotation
             )
           }
         />
@@ -103,7 +103,7 @@ const TextPopup = () => {
           onClick={() =>
             createTextAnnotationAndSelect(
               dispatch,
-              Annotations.TextSquigglyAnnotation,
+              Annotations.TextSquigglyAnnotation
             )
           }
         />
@@ -113,7 +113,7 @@ const TextPopup = () => {
           onClick={() =>
             createTextAnnotationAndSelect(
               dispatch,
-              Annotations.TextStrikeoutAnnotation,
+              Annotations.TextStrikeoutAnnotation
             )
           }
           dataElement="textStrikeoutToolButton"
@@ -122,9 +122,7 @@ const TextPopup = () => {
         <ActionButton
           title="tool.Link"
           img="icon-tool-link"
-          onClick={() =>
-            dispatch(actions.openElement('linkModal'))
-          }
+          onClick={() => dispatch(actions.openElement("linkModal"))}
           dataElement="linkButton"
         />
         {core.isCreateRedactionEnabled() && (
@@ -135,7 +133,7 @@ const TextPopup = () => {
             onClick={() =>
               createTextAnnotationAndSelect(
                 dispatch,
-                Annotations.RedactionAnnotation,
+                Annotations.RedactionAnnotation
               )
             }
           />
