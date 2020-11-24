0608b79eb7c581d08993246b0692e2af1c3a4f73
Ahtsham Raziq <ahtsham.raziq@filestage.io>
2020-11-25 04:11:18 +0500

    fix search overlay

--- src/components/SearchOverlay/SearchOverlay.js
+++ src/components/SearchOverlay/SearchOverlay.js
@@ -433,6 +433,7 @@ class SearchOverlay extends React.PureComponent {
       isSearchPanelDisabled,
       results,
       searchValue,
+      activeResultIndex,
       isWildCardSearchDisabled,
     } = this.props;
 
